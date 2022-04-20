fc264f0cb20f250b212677163d39ee71675ebcf0
youngmikec <michaelozor15@gmail.com>
2022-04-20 16:34:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to Foodies-Palace</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
