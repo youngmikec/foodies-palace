bdf2a09058358f4d65a1fa7252832560b91bb08c
Kennedy <kennyohia@gmail.com>
2022-04-25 13:41:56 -0700

    About page: have issues with the css

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import '/About.css'
+
+function About() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Welcome to Foodies-Palace</h1>
+        </header>
+      </div>
+    );
+  }
+  
+  export default About;
+  
+
--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,23 @@ const About = () => {
     return (
         <div className="App">
             <h1>Welcome to About page</h1>
+
+            <p className='p1'> Foodies palace, located in the booming mega city of Lagos,the heart of business and commerce in the country 
+                seeks to serve the populace both home and abroad with mouth-watering cuisines deserving of Kings and Queens.
+
+            </p>
+
+            <p classname = 'p2'>
+                Foodies palace was established by world-renowned chef, Chef Mike and we have been in the business of meeting the culinary needs of the
+                of the people of Lagos for the past six years recording a very large number of positive reviews across the soccial media space. We have gained 
+                international recognition, after making Forbes' list of <em>"Top 20 restaurants to Visit in the world"</em> and have also hosted The Vice President of Nigeria.
+
+            </p>
+
+            <p classname = 'p3'>With our very pocket-friendly offers, Foodies palace is sure to take your taste buds on a culinary journey of mouth-watering and finger-licking pleasure</p>
         </div>
+
+
     )
 };
 
